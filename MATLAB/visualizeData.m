01216ee980bf61b2e642e15398da3955f8f06570
Eric <ewells@ualberta.ca>
2019-01-28 15:47:00 -0700

    Added variable classes to visualize data

--- MATLAB/visualizeData.m
+++ MATLAB/visualizeData.m
@@ -1,48 +1,34 @@
-function [g] = visualizeData(data)
+function [g] = visualizeData(data, numclass)
     close all
-%     fileNamePath = strcat('../data/',fileName);
+    
+%     fileNamePath = strcat('../data/',"1450_fake8classes.csv");
 %     data = csvread(fileNamePath);
     pcacoeff = pca(data(:,1:end-1), 'NumComponents', 3);
     
-    y0 = data(:,end) == 0;
-    y1 = data(:,end) == 1;
-    y2 = data(:,end) == 2;
-    y3 = data(:,end) == 3;
+    Y = [];
+    X = {};
+    PCAX = {};
+    for i=1:numclass
+        Y(:,i) = data(:,end) == i-1;
+        X{i} = data(find(Y(:,i)),1:end-1);
+        PCAX{i} = X{i}*pcacoeff;
+    end
     
-    x0 = data(find(y0),1:end-1);
-    x1 = data(find(y1),1:end-1);
-    x2 = data(find(y2),1:end-1);
-    x3 = data(find(y3),1:end-1);
-    
-    pcax0 = x0 * pcacoeff;
-    pcax1 = x1 * pcacoeff;
-    pcax2 = x2 * pcacoeff;
-    pcax3 = x3 * pcacoeff;
-    
+    outputlabels = {'Rest', 'Up', 'Down', 'Squeeze', 'Left', 'Right', 'Supination', 'Pronation'};
     figure(1)
-    
-    scatter3(pcax0(:,1), pcax0(:,2), pcax0(:,3), 'o')
+    scatter3(PCAX{1}(:,1), PCAX{1}(:,2), PCAX{1}(:,3), 'o')
     hold on
-    scatter3(pcax1(:,1), pcax1(:,2), pcax1(:,3), 'o')
-    scatter3(pcax2(:,1), pcax2(:,2), pcax2(:,3), 'o')
-    scatter3(pcax3(:,1), pcax3(:,2), pcax3(:,3), 'o')
+    for i=2:numclass
+        scatter3(PCAX{i}(:,1), PCAX{i}(:,2), PCAX{i}(:,3), 'o')  
+    end
     grid on
-    legend(["Rest", "Up", "Down", "Squeeze"])
+    legend(outputlabels)
 
-    
-    figure(2)
     labels = {'FSR 1', 'FSR 2', 'FSR 3', 'FSR 4', 'FSR 5', 'FSR 6', 'FSR 7', 'FSR 8', 'mean'};
+    figure(2)
 
     for ii = 1:length(data(:,10))
-        if (data(ii,10) == 0)
-            name{ii,1} = 'Rest';
-        elseif (data(ii,10) == 1)
-            name{ii,1} = 'Up';
-        elseif (data(ii,10) == 2)
-            name{ii,1} = 'Down';
-        else
-            name{ii,1} = 'Squeeze';
-        end
+        name{ii,1} = outputlabels{data(ii,10)+1};
     end
 
     parallelcoords(data(:,1:9),'Group',name,'Labels',labels)
