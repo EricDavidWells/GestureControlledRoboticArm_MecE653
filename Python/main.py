8f7a385c5f7ae1c489c44ed0bfe0954ca31c5ac3
Unknown <ewells@ualberta.ca>
2019-03-03 21:26:20 -0700

    Added serial to python, changed how arduino reads serial

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,3 @@
-from sklearn import svm
 from sklearn.decomposition import PCA
 from sklearn import svm
 import csv
@@ -6,7 +5,8 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
-
+import serial
+import time
 
 def read_csv(filename):
     """
@@ -56,20 +56,53 @@ def plot_pca(X, Y):
     Xpca = pca.fit_transform(X)
 
     ax = plt.axes(projection='3d')
-
     for i in range(0, int(Y.max())+1):
         Xtemp = Xpca[Y==i]
         ax.scatter3D(Xtemp[:, 0], Xtemp[:, 1], Xtemp[:, 2], cmap='Greens')
+
     return ax, pca.explained_variance_ratio_
 
 
+def realtime(ser, model, t):
+    """
+    Realtime control of robot arm connected to serial port, continuously sends 's' character to recieve one line of
+    data poitns from arduino, makes prediction using model and sends prediction to arduino
+
+    :param ser: serial port object
+    :param model: model used for prediction, must have a .predict method
+    :param t: time in seconds to stay in realtime control
+    """
+    starttime = time.time()
+    ser.write('+\n'.encode())
+
+    counter = 0
+    while time.time()-starttime < t:
+        ser.write('s\n'.encode())
+        line = ser.readline()
+        line = line.decode()
+        line = line.replace('\n', '')
+        line = line.replace('\r', '')
+        line = line.split(',')
+        data = [float(x) for x in line[0:-1]]
+
+        pred = model.predict([data])
+        ser.write(('y'+str(pred)+'\n').encode())
+
+        print(data)
+        counter += 1
+
+    ser.write('-\n'.encode())
+
+
 # read in data
+print("Reading in Data: ")
 datafilename = "1450_RicoGrandFinalPrez.csv"
 os.chdir("..")
 datafilepath = os.path.abspath(os.curdir) + "\\data\\" + datafilename
 data = read_csv(datafilepath)
 data = np.array(data)
 
+print("Visualizing Data: ")
 # split data into inputs and outputs
 [X, Y, _, _] = data_split(data, 1)
 
@@ -78,11 +111,22 @@ plt.figure(1)
 pca_ax, var = plot_pca(X, Y)
 plt.show()
 
+print("Training Model: ")
 # split data into train and test
 [Xtrain, Ytrain, Xtest, Ytest] = data_split(data, 0.8)
 
 # train model
 model = svm.SVC(kernel='rbf', gamma='scale')
 model.fit(Xtrain, Ytrain)
-print(model.score(Xtest, Ytest))
+print("model accuracy: ", model.score(Xtest, Ytest))
 
+print("Connecting to Serial: ")
+# connect to serial
+ser = serial.Serial('COM6', 115200)
+time.sleep(3)
+
+print("Starting RealTime Control: ")
+# run real time control
+realtime(ser, model, 5)
+
+ser.close()
