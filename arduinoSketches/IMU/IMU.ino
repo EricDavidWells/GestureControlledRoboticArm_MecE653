45f0cd6bad40f401753dd519092580e274db2c73
Mark Sherstan <mshersta@ualberta.ca>
2019-03-09 07:31:53 -0700

    Update IMU sketch to only read I2C and send bytes over serial.

--- arduinoSketches/IMU/IMU.ino
+++ arduinoSketches/IMU/IMU.ino
@@ -1,134 +1,43 @@
 //Include I2C library and declare variables
 #include <Wire.h>
 
-long dtTimer;
 int temperature;
-double accelPitch;
-double accelRoll;
-long acc_x, acc_y, acc_z;
-double accel_x, accel_y, accel_z;
-double gyroRoll, gyroPitch, gyroYaw;
+int acc_x, acc_y, acc_z;
 int gyro_x, gyro_y, gyro_z;
-long gyro_x_cal, gyro_y_cal, gyro_z_cal;
-double rotation_x, rotation_y, rotation_z;
-double freq, dt;
-double tau = 0.98;
-double roll = 0;
-double pitch = 0;
 unsigned long timer = 0;
-long loopTime = 5000;   // microseconds
+long loopTimeMicroSec = 5000;
 
-// 250 deg/s --> 131, 500 deg/s --> 65.5, 1000 deg/s --> 32.8, 2000 deg/s --> 16.4
-long scaleFactorGyro = 65.5;
-
-// 2g --> 16384 , 4g --> 8192 , 8g --> 4096, 16g --> 2048
-long scaleFactorAccel = 8192;
 
 
 void setup() {
-  // Start
+  // Start I2C and serial port
   Wire.begin();
   Serial.begin(115200);
 
-  // Setup the registers of the MPU-6050 and start up
-  setup_mpu_6050_registers();
+  // Setup the registers of the MPU-6050
+  setupMPU6050();
 
-  // Calibration
-  //Serial.println("Calibrating gyro, place on level surface and do not move.");
-
-  // // Take 2000 readings for each coordinate and then find average offset
-  for (int cal_int = 0; cal_int < 2000; cal_int ++){
-    read_mpu_6050_data();
-    gyro_x_cal += gyro_x;
-    gyro_y_cal += gyro_y;
-    gyro_z_cal += gyro_z;
-
-    delay(3);
-  }
-
-  // Average the values
-  gyro_x_cal /= 2000;
-  gyro_y_cal /= 2000;
-  gyro_z_cal /= 2000;
-
-  // Display headers
-  // Serial.print("\nNote 1: Yaw is not filtered and will drift!\n");
-  // Serial.print("\nNote 2: Make sure sampling frequency is ~200 Hz\n");
-  // Serial.print("Sampling Frequency (Hz)\t\t");
-  // Serial.print("Roll (deg)\t\t");
-  // Serial.print("Pitch (deg)\t\t");
-  // Serial.print("Yaw (deg)\t\t\n");
-  // delay(2000);
-
-  // Reset the timers
-  dtTimer = micros();
+  // Reset the timer
   timer = micros();
 }
 
 
+
 void loop() {
-  timeSync(loopTime);
+  // Stabilize sampling rate
+  timeSync(loopTimeMicroSec);
 
-  freq = 1/((micros() - dtTimer) * 1e-6);
-  dtTimer = micros();
-  dt = 1/freq;
+  // Read the raw data from MPU-6050
+  readMPU6050();
 
-  // Read the raw acc data from MPU-6050
-  read_mpu_6050_data();
-
-  // Subtract the offset calibration value
-  gyro_x -= gyro_x_cal;
-  gyro_y -= gyro_y_cal;
-  gyro_z -= gyro_z_cal;
-
-  // Convert to instantaneous degrees per second
-  rotation_x = (double)gyro_x / (double)scaleFactorGyro;
-  rotation_y = (double)gyro_y / (double)scaleFactorGyro;
-  rotation_z = (double)gyro_z / (double)scaleFactorGyro;
-
-  // Convert to g force
-  accel_x = (double)acc_x / (double)scaleFactorAccel;
-  accel_y = (double)acc_y / (double)scaleFactorAccel;
-  accel_z = (double)acc_z / (double)scaleFactorAccel;
-
-  // Complementary filter
-  accelPitch = atan2(accel_y, accel_z) * RAD_TO_DEG;
-  accelRoll = atan2(accel_x, accel_z) * RAD_TO_DEG;
-
-  pitch = (tau)*(pitch + rotation_x*dt) + (1-tau)*(accelPitch);
-  roll = (tau)*(roll - rotation_y*dt) + (1-tau)*(accelRoll);
-
-  gyroPitch += rotation_x*dt;
-  gyroRoll -= rotation_y*dt;
-  gyroYaw += rotation_z*dt;
-
-  // Visualize just the roll
-  // Serial.print(roll); Serial.print(",");
-  // Serial.print(gyroRoll); Serial.print(",");
-  // Serial.println(accelRoll);
-
-  // Visualize just the pitch
-  // Serial.print(pitch); Serial.print(",");
-  // Serial.print(gyroPitch); Serial.print(",");
-  // Serial.println(accelPitch);
-
-  // Data out serial monitor
-  // Serial.print(freq,0);   Serial.print(",");
-  // Serial.print(roll,1);   Serial.print(",");
-  // Serial.print(pitch,1);  Serial.print(",");
-  // Serial.println(gyroYaw,1);
-
-  // Send pitch values to Python
-  int val0 = int(pitch);
-  int val1 = int(gyroPitch);
-  int val2 = int(accelPitch);
-  int val3 = int(freq);
-  writeBytes(&val0, &val1, &val2, &val3);
-
+  // Send raw values to Python
+  writeBytes(&acc_x, &acc_y, &acc_z, &gyro_x, &gyro_y, &gyro_z);
 }
 
 
+
 void timeSync(unsigned long deltaT){
+  // Calculate required delay to run at 200 Hz
   unsigned long currTime = micros();
   long timeToDelay = deltaT - (currTime - timer);
 
@@ -143,22 +52,30 @@ void timeSync(unsigned long deltaT){
 }
 
 
-void writeBytes(int* data1, int* data2, int* data3, int* data4){
+
+void writeBytes(int* data1, int* data2, int* data3, int* data4, int* data5, int* data6){
+  // Cast to a byte pointer
   byte* byteData1 = (byte*)(data1);
   byte* byteData2 = (byte*)(data2);
   byte* byteData3 = (byte*)(data3);
   byte* byteData4 = (byte*)(data4);
+  byte* byteData5 = (byte*)(data5);
+  byte* byteData6 = (byte*)(data6);
 
-  byte buf[10] = {0x9F, 0x6E,
+  // Byte array with header for transmission
+  byte buf[14] = {0x9F, 0x6E,
                  byteData1[0], byteData1[1],
                  byteData2[0], byteData2[1],
                  byteData3[0], byteData3[1],
-                 byteData4[0], byteData4[1]};
-  Serial.write(buf, 10);
+                 byteData4[0], byteData4[1],
+                 byteData5[0], byteData5[1],
+                 byteData6[0], byteData6[1]};
+  Serial.write(buf, 14);
 }
 
 
-void read_mpu_6050_data() {
+
+void readMPU6050() {
   //Subroutine for reading the raw data
   Wire.beginTransmission(0x68);
   Wire.write(0x3B);
@@ -177,23 +94,24 @@ void read_mpu_6050_data() {
 }
 
 
-void setup_mpu_6050_registers() {
+
+void setupMPU6050() {
   //Activate the MPU-6050
   Wire.beginTransmission(0x68);
   Wire.write(0x6B);
   Wire.write(0x00);
   Wire.endTransmission();
 
-  // Configure the accelerometer
-  // Wire.write(0x__);
+  // Configure the accelerometer (address 0x1C)
+  // Set full scale range, Wire.write(0x__);
   // Wire.write; 2g --> 0x00, 4g --> 0x08, 8g --> 0x10, 16g --> 0x18
   Wire.beginTransmission(0x68);
   Wire.write(0x1C);
   Wire.write(0x08);
   Wire.endTransmission();
 
-  // Configure the gyro
-  // Wire.write(0x__);
+  // Configure the gyro (address 0x1B)
+  // Set full scale range, Wire.write(0x__);
   // 250 deg/s --> 0x00, 500 deg/s --> 0x08, 1000 deg/s --> 0x10, 2000 deg/s --> 0x18
   Wire.beginTransmission(0x68);
   Wire.write(0x1B);
