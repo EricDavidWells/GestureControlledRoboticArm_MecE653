e89e5eefc5fff0171d1b9e4fc2b93e7b302fc9f3
Mark Sherstan <mshersta@ualberta.ca>
2019-03-09 07:59:39 -0700

    Begin treansfer from Arduino to python. Wrote gyro calibration function.

--- arduinoSketches/IMU/anglePlotter.py
+++ arduinoSketches/IMU/anglePlotter.py
@@ -7,7 +7,7 @@ import struct
 import copy
 
 
-class serialPlot:
+class DAQ:
     def __init__(self, serialPort, serialBaud, dataNumBytes, numSignals):
         self.port = serialPort
         self.baud = serialBaud
@@ -18,8 +18,6 @@ class serialPlot:
 
         if dataNumBytes == 2:
             self.dataType = 'h'
-        elif dataNumBytes == 4:
-            self.dataType = 'f'
 
         self.data = []
         for i in range(numSignals):
@@ -33,15 +31,11 @@ class serialPlot:
             print("Failed to connect with " + str(serialPort) + ' at ' + str(serialBaud) + ' BAUD.')
 
     def readSerialStart(self):
-        print("Do not move! Gyro calibrating...")
-        for ii in range(6):
-            print('.', end='', flush=True)
-            time.sleep(2)
-        print()
+        time.sleep(1.0)
         self.serialConnection.reset_input_buffer()
 
     def getSerialData(self):
-        if (int(self.serialConnection.read().hex(),16) is 159) and (int(self.serialConnection.read().hex(),16) is 110):
+        if (struct.unpack('B', self.serialConnection.read())[0] is 0x9F) and (struct.unpack('B', self.serialConnection.read())[0] is 0x6E):
             self.rawData = self.serialConnection.read(self.numSignals * self.dataNumBytes)
 
             privateData = copy.deepcopy(self.rawData[:])
@@ -60,44 +54,113 @@ class serialPlot:
         print('Disconnected...')
 
 
+
+def calibrateGyro(N):
+    # Display message
+    print("Calibrating gyro with " + str(N) + "points. Do not move!")
+
+    # Initialize
+    gyro_x_cal = 0
+    gyro_y_cal = 0
+    gyro_z_cal = 0
+
+    # Take N readings for each coordinate adding to itself
+    for ii in range(N):
+        data = s.getSerialData()
+        gyro_x_cal += data[0];
+        gyro_y_cal += data[1];
+        gyro_z_cal += data[2];
+
+    # Find average value
+    gyro_x_cal /= N;
+    gyro_y_cal /= N;
+    gyro_z_cal /= N;
+
+    # Return array of offsets
+    calibration = [gyro_x_cal, gyro_y_cal, gyro_z_cal]
+    print("Calibration complete")
+
+    return calibration
+
+
+# def compFilter(gyro_x, gyro_y, gyro_z, acc_x, acc_y, acc_z):
+#     # 250 deg/s --> 131, 500 deg/s --> 65.5, 1000 deg/s --> 32.8, 2000 deg/s --> 16.4
+#     long scaleFactorGyro = 65.5;
+#
+#     # 2g --> 16384 , 4g --> 8192 , 8g --> 4096, 16g --> 2048
+#     long scaleFactorAccel = 8192;
+#
+#     tau = 0.98
+#
+#     freq = 1/((micros() - dtTimer) * 1e-6);
+#     dtTimer = micros();
+#     dt = 1/freq;
+#
+#     // Subtract the offset calibration value
+#     gyro_x -= gyro_x_cal;
+#     gyro_y -= gyro_y_cal;
+#     gyro_z -= gyro_z_cal;
+#
+#     // Convert to instantaneous degrees per second
+#     rotation_x = (double)gyro_x / (double)scaleFactorGyro;
+#     rotation_y = (double)gyro_y / (double)scaleFactorGyro;
+#     rotation_z = (double)gyro_z / (double)scaleFactorGyro;
+#
+#     // Convert to g force
+#     accel_x = (double)acc_x / (double)scaleFactorAccel;
+#     accel_y = (double)acc_y / (double)scaleFactorAccel;
+#     accel_z = (double)acc_z / (double)scaleFactorAccel;
+#
+#     // Complementary filter
+#     accelPitch = atan2(accel_y, accel_z) * RAD_TO_DEG;
+#     accelRoll = atan2(accel_x, accel_z) * RAD_TO_DEG;
+#
+#     pitch = (tau)*(pitch + rotation_x*dt) + (1-tau)*(accelPitch);
+#     roll = (tau)*(roll - rotation_y*dt) + (1-tau)*(accelRoll);
+#
+#     gyroPitch += rotation_x*dt;
+#     gyroRoll -= rotation_y*dt;
+#     gyroYaw += rotation_z*dt;
+#
+
 def main():
     portName = '/dev/cu.wchusbserial1420'
     baudRate = 115200
     dataNumBytes = 2
     numSignals = 4
-    dataPoints = 2000
 
-    s = serialPlot(portName, baudRate, dataNumBytes, numSignals)
+    calibrationPoints = 1000;
+
+    s = DAQ(portName, baudRate, dataNumBytes, numSignals)
     s.readSerialStart()
 
-    pitch = []
-    gyroPitch = []
-    accelPitch = []
-    freq = []
+    cal = calibrateGyro(calibrationPoints):
 
-    for ii in range(dataPoints):
-        data = s.getSerialData()
-        print(data, ii)
-
-        pitch.append(data[0])
-        gyroPitch.append(data[1])
-        accelPitch.append(data[2])
-        freq.append(data[3])
-
-    print('Average frequency was: ' + str(round(mean(freq),2)) + ' Hz')
-
-    x = np.arange(len(pitch))
-
-    plt.plot(x,pitch,'-r',linewidth=5,label='Complementary Filter')
-    plt.plot(x,gyroPitch,'-g',linewidth=2,label='Gyroscope Angle')
-    plt.plot(x,accelPitch,'-b',linewidth=2,label='Accelerometer Angle')
-
-    plt.xlabel('Iteration Index')
-    plt.ylabel('Angle (deg)')
-    plt.legend(loc='upper left')
+    # dataPoints = 2000 # Change to time... Same with x axis
+    #
+    # for ii in range(dataPoints):
+    #     data = s.getSerialData()
+    #     print(data, ii)
+    #
+    #     pitch.append(data[0])
+    #     gyroPitch.append(data[1])
+    #     accelPitch.append(data[2])
+    #     freq.append(data[3])
+    #
+    # print('Average frequency was: ' + str(round(mean(freq),2)) + ' Hz')
+    #
+    # x = np.arange(len(pitch))
+    #
+    # plt.plot(x,pitch,'-k',linewidth=3,label='Complementary Filter')
+    # plt.plot(x,gyroPitch,'-g',linewidth=1,label='Gyroscope Angle')
+    # plt.plot(x,accelPitch,'-b',linewidth=1,label='Accelerometer Angle')
+    #
+    # plt.xlabel('Iteration Index')
+    # plt.ylabel('Angle (deg)')
+    # plt.legend(loc='upper left')
 
     s.close()
-    plt.show()
+    # plt.show()
 
 
 if __name__ == '__main__':
