2de24df3b0dbaf1e95752b3e549fe1de622cd576
Mark Sherstan <mshersta@ualberta.ca>
2019-03-10 16:05:38 -0600

    Completly rewrote multiplexer code.

--- arduinoSketches/multiplexer/multiplexer.ino
+++ arduinoSketches/multiplexer/multiplexer.ino
@@ -1,121 +1,94 @@
-// http://robojax.com/learn/arduino/?vid=robojax_16ch_relay_multiplexer
-// https://www.youtube.com/watch?v=lQL6dfL_rsg&feature=youtu.be
-
-/*
- * using CD74HC4067 chip to control 16 channel relay
- * with any Arduino board. This code will control 1 relay at a time
- *
- * watch video instruction for this video:https://youtu.be/lQL6dfL_rsg
- *
- * Watch main video explaining the 16 channel relay module:
- * https://youtu.be/Q9aBI4ELKC4
- *
- * Written by Ahmad Shamshiri for Robojax.com on Sunday September 30th, 2018
- * at 13:56 in Ajax, Ontario, Canada
- * Video Tutorial recorded Nov 30, 2018
- *
-  * This code is "AS IS" without warranty or liability. Free to be used as long as you keep this note intact.*
- * This code has been download from Robojax.com
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
-const int SIG = 2;// SIG pin
-const int EN = 7;// Enable pin
-const int controlPin[4] = {3, 4, 5, 6}; // 4 pins used for binary output
-
-int loopDelay = 1000;// delay in loop
-
-const int muxTable[16][4] = {
-  // s0, s1, s2, s3     channel
-    {0,  0,  0,  0}, // 0
-    {1,  0,  0,  0}, // 1
-    {0,  1,  0,  0}, // 2
-    {1,  1,  0,  0}, // 3
-    {0,  0,  1,  0}, // 4
-    {1,  0,  1,  0}, // 5
-    {0,  1,  1,  0}, // 6
-    {1,  1,  1,  0}, // 7
-    {0,  0,  0,  1}, // 8
-    {1,  0,  0,  1}, // 9
-    {0,  1,  0,  1}, // 10
-    {1,  1,  0,  1}, // 11
-    {0,  0,  1,  1}, // 12
-    {1,  0,  1,  1}, // 13
-    {0,  1,  1,  1}, // 14
-    {1,  1,  1,  1}  // 15
-};
+// Declare variables
+const int selectPins[3] = {2, 3, 4};
+unsigned long timer = 0;
+long loopTimeMicroSec = 5000;
+int FSR[8];
 
 
+void setup(){
+  // Start serial
+  Serial.begin(115200);
 
-void setup() {
-  // Robojax 16ch relay with multiplexer 20181201
-  for(int i=0; i<4; i++)
-  {
-    pinMode(controlPin[i], OUTPUT);// set pin as output
-    digitalWrite(controlPin[i], HIGH); // set initial state as HIGH
-
+  // Set up the select pins as outputs
+  for (int i=0; i<3; i++){
+    pinMode(selectPins[i], OUTPUT);
+    digitalWrite(selectPins[i], HIGH);
   }
 
+  // Connect z to analog zero
+  pinMode(A0, INPUT);
 
-    pinMode(SIG, OUTPUT);// set SIG pin as output
-    digitalWrite(SIG, LOW); // set SIG sends what should be the output
-                            // for low trigger relay it should be LOW
-                            // for HIGH trigger high it should be HIGH
-
-    pinMode(EN, OUTPUT);// set EN pin as output
-    digitalWrite(EN, LOW); // set EN in (enable pin) Low to activate the chip
-  Serial.begin(9600);
-
-   // Robojax 16ch relay with multiplexer 20181201
+  // Reset the timer
+  timer = micros();
 }
 
-void loop() {
-// Robojax 16ch relay with multiplexer 20181201
-  for(int i=0; i<16; i++)
-  {
 
-  channelControl(i);
 
-  delay(loopDelay);// wait for loopDelay ms
+void loop(){
+  // Stabilize sampling rate
+  timeSync(loopTimeMicroSec);
+
+  // Loop through all eight pins.
+  for (byte pin=0; pin<=7; pin++){
+    selectMuxPin(pin);
+    FSR[pin] = analogRead(A0);
   }
 
-  //channelControl(7);// this will turn on only relay 12
- Serial.println("===============");
-
-  // Robojax 16ch relay with multiplexer 20181201
+  // Send raw values to Python
+  writeBytes(&FSR[0], &FSR[1], &FSR[2], &FSR[3], &FSR[4], &FSR[5], &FSR[6], &FSR[7]);
 }
 
-/*
- *
- * @brief controls each relay ON or OFF
- * @param relayChannel (integer)
- * @return no return value
- */
 
-void channelControl(int relayChannel)
-{
-    // Robojax 16ch relay with multiplexer 20181201
-    digitalWrite(controlPin[0], muxTable[relayChannel][0]);
-    digitalWrite(controlPin[1], muxTable[relayChannel][1]);
-    digitalWrite(controlPin[2], muxTable[relayChannel][2]);
-    digitalWrite(controlPin[3], muxTable[relayChannel][3]);
 
-       Serial.print(relayChannel);
-       Serial.print (": ");
-       Serial.print(muxTable[relayChannel][3]);
-       Serial.print(muxTable[relayChannel][2]);
-       Serial.print(muxTable[relayChannel][1]);
-       Serial.println(muxTable[relayChannel][0]);
-   // Robojax 16ch relay with multiplexer 20181201
-}//channelControl end
+void selectMuxPin(byte pin){
+  // Set the S0, S1, and S2 pins to yield Y0-Y7
+  for (int i=0; i<3; i++){
+    if (pin & (1<<i))
+      digitalWrite(selectPins[i], HIGH);
+    else
+      digitalWrite(selectPins[i], LOW);
+  }
+}
+
+
+
+void timeSync(unsigned long deltaT){
+  // Calculate required delay to run at 200 Hz
+  unsigned long currTime = micros();
+  long timeToDelay = deltaT - (currTime - timer);
+
+  if (timeToDelay > 5000){
+    delay(timeToDelay / 1000);
+    delayMicroseconds(timeToDelay % 1000);
+  } else if (timeToDelay > 0){
+    delayMicroseconds(timeToDelay);
+  } else {}
+
+  timer = currTime + timeToDelay;
+}
+
+
+
+void writeBytes(int* data1, int* data2, int* data3, int* data4, int* data5, int* data6, int* data7, int* data8){
+  // Cast to a byte pointer
+  byte* byteData1 = (byte*)(data1);
+  byte* byteData2 = (byte*)(data2);
+  byte* byteData3 = (byte*)(data3);
+  byte* byteData4 = (byte*)(data4);
+  byte* byteData5 = (byte*)(data5);
+  byte* byteData6 = (byte*)(data6);
+  byte* byteData7 = (byte*)(data7);
+  byte* byteData8 = (byte*)(data8);
+
+  // Byte array with header for transmission
+  byte buf[18] = {0x9F, 0x6E,
+                 byteData1[0], byteData1[1],
+                 byteData2[0], byteData2[1],
+                 byteData3[0], byteData3[1],
+                 byteData4[0], byteData4[1],
+                 byteData5[0], byteData5[1],
+                 byteData6[0], byteData6[1],
+                 byteData7[0], byteData7[1],
+                 byteData8[0], byteData8[1]};
+  Serial.write(buf, 18);
+}
