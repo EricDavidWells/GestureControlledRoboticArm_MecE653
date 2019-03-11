8c361f23557bf9c36894cdd196aaca9ce96cb34d
Mark Sherstan <mshersta@ualberta.ca>
2019-03-10 23:47:31 -0600

    Rewrite main to include updated IMU and multiplexer sketches in one.

--- arduinoSketches/main/main.ino
+++ arduinoSketches/main/main.ino
@@ -1,195 +1,154 @@
-#include <Servo.h>
+//Include I2C library and declare variables
 #include <Wire.h>
 
-// Declare some variables
-Servo Shoulder;
-Servo Elbow;
-Servo Hand;
-
-int Shoulderpos = 1200;
-int Elbowpos = 1200;
-int Handpos = 3000;
-
-double accelPitch, accelRoll;
-long acc_x, acc_y, acc_z;
-long scaleFactor = 8192; // 2g --> 16384 , 4g --> 8192 , 8g --> 4096, 16g --> 2048
-double accel_x, accel_y, accel_z;
-
-const int FSR_PIN[] = {0, 1, 2, 3, 4, 5, 6, 7};
-const float VCC = 4.98;
-const float R_DIV = 5100.0;
-
-int fsrADC;
-float force[8];
-float fsrV;
-float fsrR;
-float fsrG;
-
-String label = "foo";
-String incomingString;
-int dataCount = 0;
-
-int y;
+const int selectPins[3] = {2, 3, 4};
+int FSR[8];
+int temperature;
+int acc_x, acc_y, acc_z;
+int gyro_x, gyro_y, gyro_z;
+unsigned long timer = 0;
+long loopTimeMicroSec = 5000;
 
 
 
-void setup(){
-
-
-  // Set up serial and pin mode
+void setup() {
+  // Start I2C and serial port
+  Wire.begin();
   Serial.begin(115200);
-  Serial.setTimeout(3);
-  for (int i=0;i<8;i++){
-    pinMode(FSR_PIN[i], INPUT);
+
+  // Setup the registers of the MPU-6050
+  setupMPU6050();
+
+  // Set up the select pins as outputs for multiplexer
+  for (int i=0; i<3; i++){
+    pinMode(selectPins[i], OUTPUT);
+    digitalWrite(selectPins[i], HIGH);
   }
 
-  // Place arm to initial position
-  Shoulder.attach(5);
-  Elbow.attach(4);
-  Hand.attach(11);
+  // Connect z on multiplexer to analog zero (A0)
+  pinMode(A0, INPUT);
 
-  Shoulder.writeMicroseconds(Shoulderpos);
-  Elbow.writeMicroseconds(Elbowpos);
-  Hand.writeMicroseconds(Handpos);
-  // Start the MPU 6050
-//  setup_mpu_6050_registers();
-
+  // Reset the timer
+  timer = micros();
 }
 
 
-void loop(){
-  // Read the raw acc data from MPU-6050
-//  read_mpu_6050_data();
 
-  // Convert acceleration to g force
-  accel_x = (double)acc_x / (double)scaleFactor;
-  accel_y = (double)acc_y / (double)scaleFactor;
-  accel_z = (double)acc_z / (double)scaleFactor;
+void loop() {
+  // Stabilize sampling rate
+  timeSync(loopTimeMicroSec);
 
-  // Find pitch and roll from accelerometer
-  accelPitch = atan2(accel_y, accel_z) * RAD_TO_DEG;
-  accelRoll = atan2(accel_x, accel_z) * RAD_TO_DEG;
- 
-  // Move arm based on the arm being up or down
-  if (accelPitch >= -80){
-    Elbowpos = 1150;
-  }
-  else{
-    Elbowpos = 2650;
+  // Read the raw data from MPU-6050
+  readMPU6050();
+
+  // Loop through all eight pins on multiplexer reading analog
+  for (byte pin=0; pin<=7; pin++){
+    selectMuxPin(pin);
+    FSR[pin] = analogRead(A0);
   }
 
-  // Get user/MATLAB input through serial port
-  if (Serial.available() > 0) {
-    char buf[8];
-    int len = Serial.readBytesUntil('\n', buf, 8);
-    
-    if (buf[0] == 'd'){
-      dataCount = (buf[1]-'0')*1000 + (buf[2]-'0')*100 + (buf[3]-'0')*10 + (buf[4]-'0');
-    }
-    else if (buf[0] == 's') {
-      dataCount = 1;
-    }
-    else if (buf[0] == 'y'){
-      y = (buf[1] - '0');
-    }
-    else if(buf[0] == '+'){
-       label = "on";
-    }
-    else if(buf[0] == '-'){
-       label = "off";
-    }
-    
-  }
+  // Send raw values to Python
+  writeBytes(&gyro_x, &gyro_y, &gyro_z, &acc_x, &acc_y, &acc_z,
+    &FSR[0], &FSR[1], &FSR[2], &FSR[3], &FSR[4], &FSR[5], &FSR[6], &FSR[7]);
+}
 
-  // Move the servo(s) based on the classification
-  if (y == 1){
-    Shoulderpos += 1;
-  }
-  else if (y == 2){
-    Shoulderpos -= 1;
-  }
-  if(y == 3){
-    Handpos += 1;
-  }
-  else if(y == 0){
-    Handpos -= 1;
-  }
 
-  if (label == "on"){
-  Shoulderpos = constrain(Shoulderpos, 500, 2000);
-  Elbowpos = constrain(Elbowpos, 500, 2000);
-  Handpos = constrain(Handpos, 1000, 3000);
 
-  Shoulder.writeMicroseconds(Shoulderpos);
-  Elbow.writeMicroseconds(Elbowpos);
-  Hand.writeMicroseconds(Handpos);
-  }
+void timeSync(unsigned long deltaT){
+  // Calculate required delay to run at 200 Hz
+  unsigned long currTime = micros();
+  long timeToDelay = deltaT - (currTime - timer);
 
-  // Read analog pins and convert to force
-  while (dataCount > 0){
-    for (int i=0; i<8;i++){
-      // Read pin
-      fsrADC = analogRead(FSR_PIN[i]);
+  if (timeToDelay > 5000){
+    delay(timeToDelay / 1000);
+    delayMicroseconds(timeToDelay % 1000);
+  } else if (timeToDelay > 0){
+    delayMicroseconds(timeToDelay);
+  } else {}
 
-      // Use ADC reading to calculate voltage:
-      fsrV = fsrADC * VCC / 1023.0;
+  timer = currTime + timeToDelay;
+}
 
-      // Use voltage and static resistor value to calculate FSR resistance:
-      fsrR = ((VCC - fsrV) * R_DIV) / fsrV;
 
-      // Guesstimate force based on slopes in figure 3 of FSR datasheet (conductance):
-      fsrG = 1.0 / fsrR;
 
-      // Break parabolic curve down into two linear slopes:
-      if (fsrR <= 600)
-        force[i] = (fsrG - 0.00075) / 0.00000032639;
-      else
-        force[i] =  fsrG / 0.000000642857;
-    }
-
-  // Display data [in grams] and add small pause
-  float aveforce = 0;
-      for (int i=0; i<8; i++){
-          Serial.print(force[i],0);
-          Serial.print(',');
-          aveforce += force[i];
-        }
-
-      aveforce = aveforce/8;
-      Serial.print(aveforce,0);
-      Serial.print(',');
-      Serial.println(label);
-
-      dataCount -= 1;
+void selectMuxPin(byte pin){
+  // Set the S0, S1, and S2 pins to yield Y0-Y7
+  for (int i=0; i<3; i++){
+    if (pin & (1<<i))
+      digitalWrite(selectPins[i], HIGH);
+    else
+      digitalWrite(selectPins[i], LOW);
   }
 }
 
 
-void read_mpu_6050_data() {
-  //Subroutine for reading the raw accelerometer data
+
+void writeBytes(int* data1, int* data2, int* data3, int* data4, int* data5, int* data6,
+  int* data7, int* data8, int* data9, int* data10, int* data11, int* data12, int* data13, int* data14){
+
+  // Cast to a byte pointer
+  byte* byteData1 = (byte*)(data1);     byte* byteData2 = (byte*)(data2);
+  byte* byteData3 = (byte*)(data3);     byte* byteData4 = (byte*)(data4);
+  byte* byteData5 = (byte*)(data5);     byte* byteData6 = (byte*)(data6);
+  byte* byteData7 = (byte*)(data7);     byte* byteData8 = (byte*)(data8);
+  byte* byteData9 = (byte*)(data9);     byte* byteData10 = (byte*)(data10);
+  byte* byteData11 = (byte*)(data11);   byte* byteData12 = (byte*)(data12);
+  byte* byteData13 = (byte*)(data13);   byte* byteData14 = (byte*)(data14);
+
+  // Byte array with header for transmission
+  byte buf[30] = {0x9F, 0x6E,
+                 byteData1[0],  byteData1[1],     byteData2[0],  byteData2[1],
+                 byteData3[0],  byteData3[1],     byteData4[0],  byteData4[1],
+                 byteData5[0],  byteData5[1],     byteData6[0],  byteData6[1],
+                 byteData7[0],  byteData7[1],     byteData8[0],  byteData8[1],
+                 byteData9[0],  byteData9[1],     byteData10[0], byteData10[1],
+                 byteData11[0], byteData11[1],    byteData12[0], byteData12[1],
+                 byteData13[0], byteData13[1],    byteData14[0], byteData14[1]};
+  Serial.write(buf, 30);
+}
+
+
+
+void readMPU6050() {
+  //Subroutine for reading the raw data
   Wire.beginTransmission(0x68);
   Wire.write(0x3B);
   Wire.endTransmission();
-  Wire.requestFrom(0x68, 6);
+  Wire.requestFrom(0x68, 14);
 
-  // Read data
-  while (Wire.available() < 6);
+  // Read data --> Temperature falls between acc and gyro registers
+  while(Wire.available() < 14);
   acc_x = Wire.read() << 8 | Wire.read();
   acc_y = Wire.read() << 8 | Wire.read();
   acc_z = Wire.read() << 8 | Wire.read();
+  temperature = Wire.read() <<8 | Wire.read();
+  gyro_x = Wire.read()<<8 | Wire.read();
+  gyro_y = Wire.read()<<8 | Wire.read();
+  gyro_z = Wire.read()<<8 | Wire.read();
 }
 
 
-void setup_mpu_6050_registers() {
+
+void setupMPU6050() {
   //Activate the MPU-6050
   Wire.beginTransmission(0x68);
   Wire.write(0x6B);
   Wire.write(0x00);
   Wire.endTransmission();
 
-  //Configure the accelerometer
+  // Configure the accelerometer (address 0x1C)
+  // Set full scale range, Wire.write(0x__);
+  // Wire.write; 2g --> 0x00, 4g --> 0x08, 8g --> 0x10, 16g --> 0x18
   Wire.beginTransmission(0x68);
   Wire.write(0x1C);
-  Wire.write(0x08); // 2g --> 0x00, 4g --> 0x08, 8g --> 0x10, 16g --> 0x18
+  Wire.write(0x08);
+  Wire.endTransmission();
+
+  // Configure the gyro (address 0x1B)
+  // Set full scale range, Wire.write(0x__);
+  // 250 deg/s --> 0x00, 500 deg/s --> 0x08, 1000 deg/s --> 0x10, 2000 deg/s --> 0x18
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1B);
+  Wire.write(0x08);
   Wire.endTransmission();
 }
