8d309393d682998f6a875f5a01f61a7529f52242
Mark Sherstan <mshersta@ualberta.ca>
2019-04-06 20:53:31 -0600

    Updated and added robot arm control class and tested

--- Python/robotArm.py
+++ Python/robotArm.py
@@ -1,61 +1,90 @@
 import RPi.GPIO as GPIO
-import time
 
-# Run `pinout` to see the numbers
-GPIO.setmode(GPIO.BOARD)
+class robotArm:
+    def __init__(self):
+        self.joint1Range = [500,2300]
+        self.joint2Range = [1000,2000]
+        self.joint3Range = [500,1200]
+        self.joint4Range = [1100,1100]   # [800,1800]
 
-# Set up PWM pis from GPIO and define initial position. Ensure there is a common
-# ground shared with the power supply.
-GPIO.setup(12, GPIO.OUT)
-p = GPIO.PWM(12, 100)
-p.start(11.5)
+    def startControl(self):
+        # Run `pinout` to see the numbers
+        GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(13, GPIO.OUT)
-q = GPIO.PWM(13, 100)
-q.start(11.5)
+        # Set up PWM pins on GPIO
+        GPIO.setup(12, GPIO.OUT)
+        GPIO.setup(13, GPIO.OUT)
+        GPIO.setup(18, GPIO.OUT)
+        GPIO.setup(19, GPIO.OUT)
 
-GPIO.setup(18, GPIO.OUT)
-r = GPIO.PWM(18, 100)
-r.start(11.5)
+        # Initialize all servos to center position
+        self.joint1 = GPIO.PWM(12, 50)
+        self.joint2 = GPIO.PWM(13, 50)
+        self.joint3 = GPIO.PWM(18, 50)
+        self.joint4 = GPIO.PWM(19, 50)
 
-GPIO.setup(19, GPIO.OUT)
-s = GPIO.PWM(19, 100)
-s.start(11.5)
+        # Write start position
+        self.joint1.start(7.5)
+        self.joint2.start(7.5)
+        self.joint3.start(7.5)
+        self.joint4.start(7.5)
 
-# Functions for moving each of the four DOF's on robotic arm
-def updateAngleP(angle):
-        duty = (float(angle) / 10.0) + 2.5
-        p.ChangeDutyCycle(duty)
+    def updateState(self, state):
+        # percentage / (20 ms * unit conversion)
+        dutyCycleScale = 100 / (20*1000)
 
-def updateAngleQ(angle):
-        duty = (float(angle) / 10.0) + 2.5
-        q.ChangeDutyCycle(duty)
+        # Check the different states and write a pule
+        if state == 1:
+            # Fist
+            self.joint4.ChangeDutyCycle(self.joint4Range[1]*dutyCycleScale)
+        elif state == 2:
+            # Rest
+            self.joint4.ChangeDutyCycle(self.joint4Range[0]*dutyCycleScale)
+        elif state == 3:
+            # Extension
+            self.joint1.ChangeDutyCycle(self.joint1Range[0]*dutyCycleScale)
+        elif state == 4:
+            # Flexion
+            self.joint1.ChangeDutyCycle(self.joint1Range[1]*dutyCycleScale)
+        elif state == 5:
+            # Forward
+            self.joint2.ChangeDutyCycle(self.joint2Range[0]*dutyCycleScale)
+            self.joint3.ChangeDutyCycle(self.joint3Range[1]*dutyCycleScale)
+        elif state == 6:
+            # Back
+            self.joint2.ChangeDutyCycle(self.joint2Range[1]*dutyCycleScale)
+            self.joint3.ChangeDutyCycle(self.joint3Range[0]*dutyCycleScale)
+        else:
+            print("No state found")
 
-def updateAngleR(angle):
-        duty = (float(angle) / 10.0) + 2.5
-        r.ChangeDutyCycle(duty)
+    def endControl(self):
+        # Stop writing PWM signal to servos
+        self.joint1.stop()
+        self.joint2.stop()
+        self.joint3.stop()
+        self.joint4.stop()
 
-def updateAngleS(angle):
-        duty = (float(angle) / 10.0) + 2.5
-        s.ChangeDutyCycle(duty)
+        # Clean up ports used
+        GPIO.cleanup()
 
-# Main loop running until broken
-try:
+
+def main():
+    bot = robotArm()
+    bot.startControl()
+
     while True:
-        angleP = input("Angle P: ")
-        angleQ = input("Angle Q: ")
-        angleR = input("Angle R: ")
-        angleS = input("Angle S: ")
-        print
-        updateAngleP(angleP)
-        updateAngleQ(angleQ)
-        updateAngleR(angleR)
-        updateAngleS(angleS)
-        time.sleep(1)
+        state = input('Enter State: ')
+        try:
+            state = int(state)
+            if state == -1:
+                break
 
-except KeyboardInterrupt:
-    p.stop()
-    q.stop()
-    r.stop()
-    s.stop()
-    GPIO.cleanup()
+            bot.updateState(state)
+        except:
+            print("oops")
+
+    bot.endControl()
+
+
+if __name__ == '__main__':
+    main()
--- arduinoSketches/main/main.py
+++ arduinoSketches/main/main.py
@@ -1,3 +1,4 @@
+import RPi.GPIO as GPIO
 import numpy as np
 import pandas as pd
 import serial
@@ -249,9 +250,6 @@ class Sensors:
         self.processFSRvalues()
 
     def logData(self, s, T):
-
-        starttime = time.time()
-
         # Set up data logging
         df = pd.DataFrame(columns=['Time', 'FSR1', 'FSR2', 'FSR3', 'FSR4', 'FSR5', 'FSR6',
                                    'FSR7', 'FSR8', 'FSR9', 'FSR10', 'FSR11', 'Avg'])
@@ -272,6 +270,74 @@ class Sensors:
         print("Samping rate: ", count / (time.time() - startTime), " Hz")
         s.close()
         df.to_csv('data.csv', index=None, header=True)
+
+
+class robotArm:
+    def __init__(self):
+        self.joint1Range = [500,2300]
+        self.joint2Range = [1000,2000]
+        self.joint3Range = [500,1200]
+        self.joint4Range = [1100,1100]   # [800,1800]
+
+    def startControl(self):
+        # Run `pinout` to see the numbers
+        GPIO.setmode(GPIO.BOARD)
+
+        # Set up PWM pins on GPIO
+        GPIO.setup(12, GPIO.OUT)
+        GPIO.setup(13, GPIO.OUT)
+        GPIO.setup(18, GPIO.OUT)
+        GPIO.setup(19, GPIO.OUT)
+
+        # Initialize all servos to center position
+        self.joint1 = GPIO.PWM(12, 50)
+        self.joint2 = GPIO.PWM(13, 50)
+        self.joint3 = GPIO.PWM(18, 50)
+        self.joint4 = GPIO.PWM(19, 50)
+
+        # Write start position
+        self.joint1.start(7.5)
+        self.joint2.start(7.5)
+        self.joint3.start(7.5)
+        self.joint4.start(7.5)
+
+    def updateState(self, state):
+        # percentage / (20 ms * unit conversion)
+        dutyCycleScale = 100 / (20*1000)
+
+        # Check the different states and write a pule
+        if state == 1:
+            # Fist
+            self.joint4.ChangeDutyCycle(self.joint4Range[1]*dutyCycleScale)
+        elif state == 2:
+            # Rest
+            self.joint4.ChangeDutyCycle(self.joint4Range[0]*dutyCycleScale)
+        elif state == 3:
+            # Extension
+            self.joint1.ChangeDutyCycle(self.joint1Range[0]*dutyCycleScale)
+        elif state == 4:
+            # Flexion
+            self.joint1.ChangeDutyCycle(self.joint1Range[1]*dutyCycleScale)
+        elif state == 5:
+            # Forward
+            self.joint2.ChangeDutyCycle(self.joint2Range[0]*dutyCycleScale)
+            self.joint3.ChangeDutyCycle(self.joint3Range[1]*dutyCycleScale)
+        elif state == 6:
+            # Back
+            self.joint2.ChangeDutyCycle(self.joint2Range[1]*dutyCycleScale)
+            self.joint3.ChangeDutyCycle(self.joint3Range[0]*dutyCycleScale)
+        else:
+            print("No state found")
+
+    def endControl(self):
+        # Stop writing PWM signal to servos
+        self.joint1.stop()
+        self.joint2.stop()
+        self.joint3.stop()
+        self.joint4.stop()
+
+        # Clean up ports used
+        GPIO.cleanup()
 
 
 def main():
